2f6ce849d4c8cc8ac70fe558f7cca55e02928e77
Ankit Shaw <shawincible@gmail.com>
2026-07-07 22:59:59 +0530

    Update 01_database_setup.sql
    Added database setup script

--- sql/01_database_setup.sql
+++ sql/01_database_setup.sql
@@ -1,1 +1,21 @@
+/*
+===============================================================================
+Project      : Library Management System
+File         : 01_database_setup.sql
+Author       : Ankit Shaw
+Database     : PostgreSQL
 
+Description:
+Creates the database for the Library Management System.
+
+Execution Order:
+1. Execute this script first.
+
+===============================================================================
+*/
+
+-- ============================================================================
+-- Create Database
+-- ============================================================================
+
+CREATE DATABASE library_project;
