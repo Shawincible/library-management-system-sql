0b6f8019cdd983e4b5d2a5846a2a884fad1f035f
Ankit Shaw <shawincible@gmail.com>
2026-07-12 11:26:12 +0530

    Update 04_data_population.sql

--- sql/04_data_population.sql
+++ sql/04_data_population.sql
@@ -1,6 +1,6 @@
 /*
 ===============================================================================
-Project      : Library Management System
+Project      : Enterprise Library Management System
 File         : 04_data_population.sql
 Author       : Ankit Shaw
 Database     : PostgreSQL
